e91b40b95aea0ba1da52dc6daded4cb61561b7ab
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-12-15 10:25:30 +0100

    empty project

---
+++ src/main/java/de/tekup/recipe/RecipeAppDApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAppDApplication.class, args);
+	}
+
+}
